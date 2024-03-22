4e1d489a6adfec9037bd96a02ab041a1947a6907
Jefferson <jefferson.sg91@hotmail.com>
2024-03-22 20:36:44 -0300

    alterado para usar xml

--- src/main/java/api/CepApiConsumer.java
+++ src/main/java/api/CepApiConsumer.java
@@ -1,9 +1,11 @@
 package api;
 
-import com.google.gson.Gson;
+
 import model.Address;
 
-import java.io.IOException;
+import javax.swing.*;
+import javax.xml.bind.JAXBContext;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -11,31 +13,52 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 
 public class CepApiConsumer {
-    public Address getAddress(String cep) throws IOException, InterruptedException {
-        //configura a URL da api
-        var url = "http://viacep.com.br/ws/".concat(cep).concat("/json/");
+    public Address getAddress(String cep) {
+        try {
+            //configura a URL da api
+            var url = "http://viacep.com.br/ws/".concat(cep).concat("/xml/");
 
-        //cria o objeto responsavel pela request
-        var request = HttpRequest.newBuilder()
-                .GET()
-                .uri(URI.create(url));
+            //cria o objeto responsavel pela request
+            var request = HttpRequest.newBuilder()
+                    .GET()
+                    .uri(URI.create(url));
 
-        //cria o client para o request
-        var httpCLient = HttpClient.newBuilder()
-                .connectTimeout(Duration.ofSeconds(8))
-                .build();
+            //cria o client para o request
+            var httpCLient = HttpClient.newBuilder()
+                    .connectTimeout(Duration.ofSeconds(8))
+                    .build();
 
-        //cria o objeto response da request
-        var response = httpCLient.send(request.build(), HttpResponse.BodyHandlers.ofString());
+            //cria o objeto response da request
+            var response = httpCLient.send(request.build(), HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+            // Verifica se a resposta foi bem-sucedida
+            if (response.statusCode() != 200) {
+                throw new RuntimeException("O CEP digitado é inválido.");
+            }
 
-        //biblioteca que converter o formato JSON em objeto Java
-        var gson = new Gson();
+            //tenta criar um objeto reader com o conteudo da resposta XML convertido para uma cadeia de caracteres
+            var reader = new StringReader(response.body());
 
-        //conversao do formato JSON para o objeto address
-        var address = gson.fromJson(response.body(), Address.class);
+            //biblioteca que converte o formato XML em objeto Java
+            var jaxbContext = JAXBContext.newInstance(Address.class);
 
-        return address;
+            //cria um objeto unmarshaller que permite a desserialização do reader
+            var unmarshaller = jaxbContext.createUnmarshaller();
+
+            // Verifica se a tag <erro> está presente no XML
+            if (response.body().contains("<erro>true</erro>")) {
+                throw new RuntimeException("Esse CEP não existe.");
+            }
+
+            //retorna um objeto address com a restauração do unmarshaller
+            return (Address) unmarshaller.unmarshal(reader);
+        } catch (Exception ex) {
+            throwError(ex.getMessage());
+        }
+        return null;
+    }
+
+    private void throwError(String msg) {
+        JOptionPane.showMessageDialog(null, msg.concat("\nTente novamente."));
     }
 }
--- src/main/java/model/Address.java
+++ src/main/java/model/Address.java
@@ -2,10 +2,12 @@ package model;
 
 import lombok.Data;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 @Data
+@XmlRootElement(name = "xmlcep")
 public class Address {
     private String cep, logradouro, complemento,
             bairro, localidade, uf,
             ibge, gia, ddd, siafi;
-
 }
